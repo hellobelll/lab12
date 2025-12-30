caa579a1343cb7ad7bb17afee312a485fc26c35d
belll_ <ess.fuji5566@gmail.com>
2025-12-30 15:36:16 +0700

    lab12

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 
 using namespace std;
-
+template <typename currentType>
+void mySwap(currentType &,currentType &);
 int main(){
 	int x, y;
 	string a, b;
@@ -29,4 +30,10 @@ int main(){
 
 	return 0;
 }
-
+template <typename currentType>
+void mySwap(currentType &x,currentType &y){
+	currentType z;
+	z=x;
+	x=y;
+	y=z;
+}
